d3091d986bad4fe24d6bd172db0d47cac8fd9729
Ben Sandberg <ben@pdxfixit.com>
2012-08-14 09:26:35 +0000

    updates and views

---
+++ views/sites/view.html.php
@@ -0,0 +1,19 @@
+<?php
+
+defined('_JEXEC') or die('Access Restricted');
+
+jimport('joomla.application.component.view');
+
+class CommandViewSites extends JView {
+
+    public function display($tpl = null) {
+        JToolBarHelper::title(JText::_('COM_COMMAND_ADMINISTRATION_TITLE'), 'command');
+        JToolBarHelper::preferences('com_command', '500');
+        
+        parent::display($tpl);
+        
+        $document = JFactory::getDocument();
+        $document->setTitle(JText::_('COM_COMMAND_SITES'));
+    }
+
+}
